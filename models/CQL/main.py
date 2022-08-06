2fcbe154f9ac00e41cb4451a79cb9176471dee0d
hezhi <hezhi@yy.com>
2022-08-06 12:30:29 +0800

    CQL...

---
+++ models/CQL/main.py
@@ -0,0 +1,46 @@
+import gym
+import matplotlib.pyplot as plt
+
+import os, sys
+root_dir_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+sys.path.append(root_dir_path)
+
+from utils.helper import *
+from utils.replay_buffer import *
+from utils.const import *
+
+from sac_continuous import *
+
+env_name = 'Pendulum-v1'
+env = gym.make(env_name)
+state_dim = env.observation_space.shape[0]
+action_dim = env.action_space.shape[0]
+action_bound = env.action_space.high[0]  # 动作最大值
+random.seed(0)
+np.random.seed(0)
+env.seed(0)
+torch.manual_seed(0)
+
+actor_lr = 3e-4
+critic_lr = 3e-3
+alpha_lr = 3e-4
+num_episodes = 100
+hidden_dim = 128
+gamma = 0.99
+tau = 0.005  # 软更新参数
+buffer_size = 100000
+minimal_size = 1000
+batch_size = 64
+target_entropy = -env.action_space.shape[0]
+
+replay_buffer = ReplayBuffer(buffer_size)
+agent = SACContinuous(state_dim, hidden_dim, action_dim, action_bound, actor_lr, critic_lr, alpha_lr, target_entropy, tau, gamma, DEVICE)
+
+return_list = train_off_policy_agent(env, agent, EPOCH_NUM, num_episodes, replay_buffer, minimal_size, batch_size)
+
+episodes_list = list(range(len(return_list)))
+plt.plot(episodes_list, return_list)
+plt.xlabel('Episodes')
+plt.ylabel('Returns')
+plt.title('SAC on {}'.format(env_name))
+plt.show()
